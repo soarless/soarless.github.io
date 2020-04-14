3764c6ecee5a521769ecc2f563124c4e6012ed13
soarless <353033712@qq.com>
2020-04-14 22:00:41 +0800

    Site updated: 2020-04-14 22:00:41

--- js/main.js
+++ js/main.js
@@ -227,23 +227,23 @@ $(function () {
   /**
    * BOOKMARK 書簽
    */
-  $('#bookmark-it').on('click', function () {
-    if (window.sidebar && window.sidebar.addPanel) { // Mozilla Firefox Bookmark
-      window.sidebar.addPanel(document.title, window.location.href, '')
-    } else if (window.external && ('AddFavorite' in window.external)) { // IE Favorite
-      window.external.AddFavorite(location.href, document.title)
-    } else if (window.opera && window.print) { // Opera Hotlist
-      this.title = document.title
-      return true
-    } else { // webkit - safari/chrome
-      if (isSnackbar) {
-        var bookmarkText = GLOBAL_CONFIG.Snackbar.bookmark.message_prev + ' ' + (navigator.userAgent.toLowerCase().indexOf('mac') !== -1 ? 'Command/Cmd' : 'CTRL') + '+ D ' + GLOBAL_CONFIG.Snackbar.bookmark.message_next + '.'
-        snackbarShow(bookmarkText)
-      } else {
-        alert(GLOBAL_CONFIG.bookmark.message_prev + ' ' + (navigator.userAgent.toLowerCase().indexOf('mac') !== -1 ? 'Command/Cmd' : 'CTRL') + '+ D ' + GLOBAL_CONFIG.bookmark.message_next + '.')
-      }
-    }
-  })
+  // $('#bookmark-it').on('click', function () {
+  //   if (window.sidebar && window.sidebar.addPanel) { // Mozilla Firefox Bookmark
+  //     window.sidebar.addPanel(document.title, window.location.href, '')
+  //   } else if (window.external && ('AddFavorite' in window.external)) { // IE Favorite
+  //     window.external.AddFavorite(location.href, document.title)
+  //   } else if (window.opera && window.print) { // Opera Hotlist
+  //     this.title = document.title
+  //     return true
+  //   } else { // webkit - safari/chrome
+  //     if (isSnackbar) {
+  //       var bookmarkText = GLOBAL_CONFIG.Snackbar.bookmark.message_prev + ' ' + (navigator.userAgent.toLowerCase().indexOf('mac') !== -1 ? 'Command/Cmd' : 'CTRL') + '+ D ' + GLOBAL_CONFIG.Snackbar.bookmark.message_next + '.'
+  //       snackbarShow(bookmarkText)
+  //     } else {
+  //       alert(GLOBAL_CONFIG.bookmark.message_prev + ' ' + (navigator.userAgent.toLowerCase().indexOf('mac') !== -1 ? 'Command/Cmd' : 'CTRL') + '+ D ' + GLOBAL_CONFIG.bookmark.message_next + '.')
+  //     }
+  //   }
+  // })
 
   /**
    * 代碼
